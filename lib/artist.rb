2c15cd97ec97b28ac319dcc5c894f1a2e27bc8b7
rockshellS <spicer.rachelle@gmail.com>
2020-12-13 17:09:13 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,45 @@
+require_relative "song.rb"
+
+
+class Artist
+
+    attr_accessor :name 
+    
+    def initialize(name)
+        @name = name
+
+    end
+
+
+    def songs
+        Song.all.select  { |song|  song  }
+    end
+
+    def add_song(song)
+      song.artist = self
+ end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    song.artist = self
+    end
+
+    def self.song_count
+        Song.all.count
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
